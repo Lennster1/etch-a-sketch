dd021087add70fb1d7d907514f2b68a9e3ef4610
Lenny <76703996+Lennster1@users.noreply.github.com>
2024-02-25 15:49:38 +0000

    add styles.css

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-for (let i = 0; i = 256; i++) {
-    const container = document.querySelector(".container");
-    const square = document.createElement("div");
-    container.appendChild(square);
-};
+for (let i = 1; (i = 256); i++) {
+  const container = document.querySelector(".container");
+  const square = document.createElement("div");
+  square.classList.toggle("square");
+  container.appendChild(square);
+}
