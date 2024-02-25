a7fe345b75541471d898de05943afa1cc181ac9c
Lenny Luo <76703996+Lennster1@users.noreply.github.com>
2024-02-25 11:07:36 -0500

    style square divs to make them display properly

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-for (let i = 1; (i = 256); i++) {
+for (let i = 0; i < 256; i++) {
   const container = document.querySelector(".container");
   const square = document.createElement("div");
   square.classList.toggle("square");
