dd5215889783e8313c47aa2cefd9e4a9e2528ce9
Lenny Luo <76703996+Lennster1@users.noreply.github.com>
2024-02-25 21:51:31 -0500

    fix resizing squares and center the div(lol)

--- main.js
+++ main.js
@@ -16,30 +16,36 @@ for (let i = 0; i < 16 ** 2; i++) {
 
 function drawSquares(sideLength) {
   const body = document.querySelector("body");
-  const container = document.createElement("div");
-  container.classList.toggle("container");
-  body.appendChild(container);
+  const container = document.querySelector(".container");
+  body.removeChild(container);
+  const container_ = document.createElement("div");
+  container_.classList.toggle("container");
+  body.appendChild(container_);
   for (let i = 0; i < sideLength * sideLength; i++) {
     const square = document.createElement("div");
     square.classList.toggle("square");
+    square.style.width = `${100 / sideLength}%`;
+    square.style.height = `${100 / sideLength}%`;
+    square.style.backgroundColor = "aqua";
     square.addEventListener("mouseover", () => {
       square.style.backgroundColor = "red";
     });
     square.addEventListener("mouseout", () => {
       square.style.backgroundColor = "aqua";
     });
-    container.appendChild(square);
+    container_.appendChild(square);
   }
 }
 
 const btn = document.querySelector("button");
 const body = document.querySelector("body");
 btn.addEventListener("click", () => {
-  let dimensions = prompt("How many squares on each side?");
+  let dimensions = parseFloat(prompt("How many squares on each side?"));
   if (dimensions > 100) {
     alert("Input must be a number less than 100");
+  } else if (typeof(dimensions) != "number") {
+    alert("Input must be a number");
   } else {
-    body.removeChild(container);
     drawSquares(dimensions);
   }
 });
