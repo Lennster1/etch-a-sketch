028312143a0bb1fc87788e4b049c964bae7e82ae
Lenny Luo <76703996+Lennster1@users.noreply.github.com>
2024-02-26 21:15:54 -0500

    finasl bv

--- main.js
+++ main.js
@@ -1,45 +1,77 @@
+let rainbowMode = false;
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 const container = document.querySelector(".container");
 for (let i = 0; i < 16 ** 2; i++) {
   const square = document.createElement("div");
   square.classList.toggle("square");
   square.style.width = `${100 / 16}%`;
   square.style.height = `${100 / 16}%`;
-  square.style.backgroundColor = "aqua";
+  square.style.backgroundColor = "#cc9999";
   square.addEventListener("mouseover", () => {
-    square.style.backgroundColor = "red";
+    square.style.backgroundColor = "aqua";
   });
   square.addEventListener("mouseout", () => {
-    square.style.backgroundColor = "aqua";
+    square.style.backgroundColor = "#cc9999";
   });
   container.appendChild(square);
 }
 
 function drawSquares(sideLength) {
-  const body = document.querySelector("body");
-  const container = document.querySelector(".container");
-  body.removeChild(container);
-  const container_ = document.createElement("div");
-  container_.classList.toggle("container");
-  body.appendChild(container_);
-  for (let i = 0; i < sideLength * sideLength; i++) {
-    const square = document.createElement("div");
-    square.classList.toggle("square");
-    square.style.width = `${100 / sideLength}%`;
-    square.style.height = `${100 / sideLength}%`;
-    square.style.backgroundColor = "aqua";
-    square.addEventListener("mouseover", () => {
-      square.style.backgroundColor = "red";
-    });
-    square.addEventListener("mouseout", () => {
-      square.style.backgroundColor = "aqua";
-    });
-    container_.appendChild(square);
+  if (rainbowMode = true) {
+    const body = document.querySelector("body");
+    const container = document.querySelector(".container");
+    body.removeChild(container);
+    const container_ = document.createElement("div");
+    container_.classList.toggle("container");
+    body.appendChild(container_);
+    for (let i = 0; i < sideLength * sideLength; i++) {
+      const square = document.createElement("div");
+      square.classList.toggle("square");
+      square.style.width = `${100 / sideLength}%`;
+      square.style.height = `${100 / sideLength}%`;
+      square.style.backgroundColor = "#cc9999";
+      square.addEventListener("mouseenter", () => {
+        square.style.backgroundColor = `rgb(${getRandomInt(
+          0,
+          255
+        )}, ${getRandomInt(0, 255)}, ${getRandomInt(0, 255)})`;
+        console.log(square.style.backgroundColor);
+      });
+      container_.appendChild(square);
+    }
+  } else {
+    const body = document.querySelector("body");
+    const container = document.querySelector(".container");
+    body.removeChild(container);
+    const container_ = document.createElement("div");
+    container_.classList.toggle("container");
+    body.appendChild(container_);
+    for (let i = 0; i < sideLength * sideLength; i++) {
+      const square = document.createElement("div");
+      square.classList.toggle("square");
+      square.style.width = `${100 / sideLength}%`;
+      square.style.height = `${100 / sideLength}%`;
+      square.style.backgroundColor = "#cc9999";
+      square.addEventListener("mouseover", () => {
+        square.style.backgroundColor = "aqua";
+      });
+      square.addEventListener("mouseout", () => {
+        square.style.backgroundColor = "#cc9999";
+      });
+      container_.appendChild(square);
+    }
   }
 }
 
-const btn = document.querySelector("button");
+const dimensionsBtn = document.querySelector("#dimensions");
+const rainbowBtn = document.querySelector("#rainbow");
 const body = document.querySelector("body");
-btn.addEventListener("click", () => {
+dimensionsBtn.addEventListener("click", () => {
   let dimensions = prompt("How many squares on each side?");
   let dimensionsNumber = Number(dimensions);
   if (dimensionsNumber > 100) {
@@ -49,4 +81,8 @@ btn.addEventListener("click", () => {
   } else {
     drawSquares(dimensions);
   }
+});
+rainbowBtn.addEventListener("click", () => {
+  rainbowMode = true
+  drawSquares(16)
 });
